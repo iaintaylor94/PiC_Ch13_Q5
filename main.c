c6f691aedb7a088a14fca145609849fd18c9c4f3
Iain Taylor <iain.taylor.94@gmail.com>
2023-09-16 14:51:29 +0000

    completed program

--- main.c
+++ main.c
@@ -1,6 +1,18 @@
+// This program demonstrates the use of a shift right macro
+
 #include <stdio.h>
 
+#define SHIFT(shift, by) (by > 0) ? (shift <<= by) : (shift >>= -by)
+
 int main(void) {
-  printf("Hello World\n");
+
+  unsigned int sft = 0b00000100; // 4
+  
+  int by = 2; // * 4
+  printf ("%d\n", SHIFT(sft, by)); // 4*4 = 16
+
+  by = -1; // / 2
+  printf ("%d\n", SHIFT(sft, by)); // 16/2 = 8
+  
   return 0;
 }
